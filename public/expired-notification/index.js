768c090c857c77b97a67abc49ae1b2c10fb48b4e
Whoosh <nhatkha1407@gmail.com>
2022-12-30 16:25:45 +0700

    Never gonna give you up

--- public/expired-notification/index.js
+++ public/expired-notification/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   const html =
-    '<main style="height: 100%;width: 100%;display: flex;position: fixed;font-size: calc(.5 * (5vh + 3vw));justify-content: center;align-items: center;margin: auto;color: red;font-weight: bold;padding:5vh;font-family: system-ui,-apple-system,\'Segoe UI\',Roboto,Helvetica,Arial,sans-serif,\'Apple Color Emoji\',\'Segoe UI Emoji\' !important;line-height: 1.3;"><h1>Server/Hosting của quý khách đã hết hạn.<br/>Quý khách vui lòng liên hệ quản trị viên để gia hạn dịch vụ.</h1><main>';
+    "<main style=\"height: 100%;width: 100%;display: flex;position: fixed;justify-content: center;align-items: center;margin: auto;font-weight: bold;padding:5vh;\"><h1 style=\"color: red;font-size: calc(.5 * (5vh + 3vw));font-family: system-ui,-apple-system,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji' !important;line-height: 1.3;\">Server/Hosting của quý khách đã hết hạn.<br/>Quý khách vui lòng liên hệ quản trị viên để gia hạn dịch vụ.</h1><main>";
   document.querySelector("body").innerHTML = html;
 });
