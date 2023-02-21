03c418076c522573565ff69e6044bd816d636606
Whoosh <nhatkha1407@gmail.com>
2023-02-21 11:44:48 +0700

    bugger

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -4,11 +4,12 @@ import { deepMerge } from '../src/js/utils/deep-merge'
 import defaultTheme from '../src/tailwind.config.js'
 
 module.exports = deepMerge(defaultTheme, {
+
   // Custom code here
+
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
   ],
-
 })
