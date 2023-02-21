d8a6f83e6a1ef99d37a735de0c86e2d68f38ed2b
Whoosh <nhatkha1407@gmail.com>
2023-02-21 11:01:25 +0700

    Trust me, I'm an engineer!... What the f*ck did just happened here?

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -9,14 +9,14 @@ console.log(libraryDir);
 export default defineNuxtConfig({
 	modules: ['@nuxtjs/tailwindcss'],
 
-  components: {
-    "dirs": [
-      {
-        "path": "../src/js/components",
-        "global": true
-      },
-    ]
-  },
+  // components: {
+  //   "dirs": [
+  //     {
+  //       "path": "../src/js/components",
+  //       "global": true
+  //     },
+  //   ]
+  // },
 
 	tailwindcss: {
 		cssPath: '@/assets/scss/app.scss',
