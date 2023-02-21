76cb9f57561f35db56db059f4ba0f1c197e9ea11
Nhật Linh <linhln.flamedia@gmail.com>
2023-02-21 16:39:43 +0700

    push code

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -6,7 +6,6 @@ const libraryDir = path.join(parentDir, 'src')
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
 
-<<<<<<< HEAD
     components: {
         dirs: [
             {
@@ -25,7 +24,8 @@ export default defineNuxtConfig({
     alias: {
         '@core/utils': libraryDir + '/js/utils',
         '@core/scss': libraryDir + '/scss',
-        '@core/images': libraryDir + '/images',
+        'core-images': libraryDir + '/images',
+        // 'core-images': fileURLToPath(new URL(libraryDir + '/images', import.meta.url)),
     },
 
     vite: {
@@ -34,34 +34,5 @@ export default defineNuxtConfig({
                 external: ['vue/server-renderer'],
             },
         },
-=======
-  components: {
-    dirs: [
-      {
-        path: libraryDir + '/js/components',
-        global: true,
-      },
-    ],
-  },
-
-  tailwindcss: {
-    cssPath: '@/assets/scss/app.scss',
-    configPath: 'tailwind.config.js',
-    viewer: false,
-  },
-
-  alias: {
-    '@core/utils': libraryDir + '/js/utils',
-    '@core/scss': libraryDir + '/scss',
-    'core-images': libraryDir + '/images',
-    // 'core-images': fileURLToPath(new URL(libraryDir + '/images', import.meta.url)),
-  },
-
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ['vue/server-renderer'],
-      },
->>>>>>> b1b3b8ee0d018381434bab49bdc3c3b883cef5fa
     },
 })
