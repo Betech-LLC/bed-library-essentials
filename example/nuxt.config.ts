ae2719b964312c2a7a32800a25761fea7558077c
Whoosh <nhatkha1407@gmail.com>
2023-02-21 22:28:09 +0700

    Update nuxt.config.ts

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
     alias: {
         '@core/utils': libraryDir + '/js/utils',
         '@core/scss': libraryDir + '/scss',
+        '@core': libraryDir + '/src',
     },
 
     vite: {
