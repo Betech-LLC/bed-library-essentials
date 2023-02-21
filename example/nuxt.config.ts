dde062a63476984dd7cd54d545cb323fab7c92f5
Whoosh <nhatkha1407@gmail.com>
2023-02-21 15:03:33 +0700

    @core/images

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
   alias: {
     '@core/utils': libraryDir + '/js/utils',
     '@core/scss': libraryDir + '/scss',
+    '@core/images': libraryDir + '/images',
   },
 
   vite: {
