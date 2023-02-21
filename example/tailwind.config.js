11369eef891259e64cb09d1ae14586102f10670e
Whoosh <nhatkha1407@gmail.com>
2023-02-21 11:29:08 +0700

    Argh! About to give up :(

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -4,6 +4,7 @@ import { deepMerge } from '../src/js/utils/deep-merge'
 import defaultTheme from '../src/tailwind.config.js'
 
 module.exports = deepMerge(defaultTheme, {
+  // Custom code here
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
