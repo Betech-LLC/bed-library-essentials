66088c7b043a8f2ed59cd20946c8ff2c3e96eafb
Nhật Linh <linhln.flamedia@gmail.com>
2023-02-27 15:07:08 +0700

    push code

--- example/pages/card.vue
+++ example/pages/card.vue
@@ -2,9 +2,10 @@
     <section class="container space-y-8">
         <div class="grid grid-cols-2 gap-x-8">
             <JCardBlog :item="post" />
+            <JCardBlog :item="post" />
         </div>
         <JCardBlog class="card-blog-row w-[800px]" :item="post" />
-        <JCardBlog class="card-blog-row w-[314px] card-blog-xs" :item="post" />
+        <JCardBlog class="card-blog-row w-[314px] card-blog-side-bar" :item="post" />
     </section>
 </template>
 
