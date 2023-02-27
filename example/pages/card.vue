5f82824126e5a4fea670e916a0384965fbf10eae
Nhật Linh <linhln.flamedia@gmail.com>
2023-02-27 14:18:47 +0700

    push code

--- example/pages/card.vue
+++ example/pages/card.vue
@@ -1,6 +1,10 @@
 <template>
-    <section class="container grid grid-cols-2 gap-x-8">
-        <CardBlog :item="post" />
+    <section class="container space-y-8">
+        <div class="grid grid-cols-2 gap-x-8">
+            <JCardBlog :item="post" />
+        </div>
+        <JCardBlog class="card-blog-row w-[800px]" :item="post" />
+        <JCardBlog class="card-blog-row w-[314px] card-blog-xs" :item="post" />
     </section>
 </template>
 
--- src/js/components/JCardBlog.vue
+++ src/js/components/JCardBlog.vue
@@ -1,16 +1,18 @@
 <template>
     <div v-if="item" class="card-blog">
-        <Picture class="card-blog-image-top" :src="item.image || '/cover.jpg'" />
+        <a href="#">
+            <JPicture class="card-blog-image-top" :src="item.image || '/cover.jpg'" />
+        </a>
         <div class="card-blog-body">
             <div class="card-blog-meta">
-                <span class="badge badge-primary">SEO Strategy</span>
+                <span class="badge badge-primary badge-sm">SEO Strategy</span>
                 <span class="card-blog-published-date">Thứ Sáu, 17/02/2023</span>
             </div>
 
-            <h3 v-if="item.title" class="card-blog-title">{{ item.title }}</h3>
+            <h3 v-if="item.title" class="card-blog-title">
+                <a href="#">{{ item.title }}</a>
+            </h3>
             <p v-if="item.description" class="card-blog-description" v-html="item.description"></p>
-
-            <a :href="link" class="card-blog-button">Xem chi tiết</a>
         </div>
     </div>
 </template>
