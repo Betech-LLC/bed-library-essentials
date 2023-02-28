657beae870a744ce564850e7a6667b1ae4807990
Whoosh <nhatkha1407@gmail.com>
2023-02-28 08:35:16 +0700

    Update JLink.vue

--- src/js/components/JLink.vue
+++ src/js/components/JLink.vue
@@ -1,5 +1,12 @@
 <template>
-  <CoreLink v-bind="$attrs">
-      <slot></slot>
-  </CoreLink>
+    <CoreLink v-bind="$attrs">
+        <slot></slot>
+    </CoreLink>
 </template>
+
+<script>
+import CoreLink from '@framework/components/CoreLink.vue'
+export default {
+    components: { CoreLink },
+}
+</script>
