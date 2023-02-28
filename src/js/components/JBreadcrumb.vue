2bb3dbff37e74b515856d970819656ef43aeec3c
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-02-28 23:34:58 +0700

    push ocde

--- src/js/components/JBreadcrumb.vue
+++ src/js/components/JBreadcrumb.vue
@@ -1,22 +1,23 @@
 <template>
-  <nav class="container" aria-label="breadcrumb" v-if="items.length">
-    <ol class="breadcrumb">
-      <template v-for="(item, index) in items" :key="index">
-        <li class="breadcrumb-item">
-          <a :href="item.link">{{ item.title }}</a>
-        </li>
-      </template>
-    </ol>
-  </nav>
+    <nav class="container" aria-label="breadcrumb" v-if="items.length">
+        <ol class="breadcrumb">
+            <template v-for="(item, index) in items" :key="index">
+                <li class="breadcrumb-item" :class="{ arrow: isArrow }">
+                    <a :href="item.link">{{ item.title }}</a>
+                </li>
+            </template>
+        </ol>
+    </nav>
 </template>
 
 <script>
 export default {
-  props: {
-    items: {
-      type: Array,
-      required: true,
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        },
+        isArrow: { type: Boolean, default: true },
     },
-  },
-};
+}
 </script>
