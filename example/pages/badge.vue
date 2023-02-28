73a67646aef84c5505fa6c2c8458874c3e119cb5
Nhật Linh <linhln.flamedia@gmail.com>
2023-02-28 16:55:56 +0700

    push code

--- example/pages/badge.vue
+++ example/pages/badge.vue
@@ -1,19 +1,60 @@
 <template>
     <div class="container py-10 space-y-10">
-        <div class="space-x-2 space-y-2">
-            <span class="badge badge-gray">Label gray</span>
-            <span class="badge badge-primary">Label primary</span>
-            <span class="badge badge-error">Label error</span>
-            <span class="badge badge-warning">Label warning</span>
-            <span class="badge badge-success">Label success</span>
-            <span class="badge badge-gray-blue">Label gray-blue</span>
-            <span class="badge badge-blue-light">Label blue-light</span>
-            <span class="badge badge-blue">Label blue</span>
-            <span class="badge badge-indigo">Label indigo</span>
-            <span class="badge badge-purple">Label purple</span>
-            <span class="badge badge-pink">Label pink</span>
-            <span class="badge badge-rose">Label rose</span>
-            <span class="badge badge-orange-dark">Label orange-dark</span>
+        <div>
+            <div class="headline-2">Size sm</div>
+            <div class="space-x-2 space-y-2">
+                <span class="badge badge-sm badge-gray">Label gray</span>
+                <span class="badge badge-sm badge-primary">Label primary</span>
+                <span class="badge badge-sm badge-error">Label error</span>
+                <span class="badge badge-sm badge-warning">Label warning</span>
+                <span class="badge badge-sm badge-success">Label success</span>
+                <span class="badge badge-sm badge-gray-blue">Label gray-blue</span>
+                <span class="badge badge-sm badge-blue-light">Label blue-light</span>
+                <span class="badge badge-sm badge-blue">Label blue</span>
+                <span class="badge badge-sm badge-indigo">Label indigo</span>
+                <span class="badge badge-sm badge-purple">Label purple</span>
+                <span class="badge badge-sm badge-pink">Label pink</span>
+                <span class="badge badge-sm badge-rose">Label rose</span>
+                <span class="badge badge-sm badge-orange-dark">Label orange-dark</span>
+            </div>
+        </div>
+
+        <div>
+            <div class="space-x-2 space-y-2">
+                <div class="headline-2">Size md</div>
+                <span class="badge badge-gray">Label gray</span>
+                <span class="badge badge-primary">Label primary</span>
+                <span class="badge badge-error">Label error</span>
+                <span class="badge badge-warning">Label warning</span>
+                <span class="badge badge-success">Label success</span>
+                <span class="badge badge-gray-blue">Label gray-blue</span>
+                <span class="badge badge-blue-light">Label blue-light</span>
+                <span class="badge badge-blue">Label blue</span>
+                <span class="badge badge-indigo">Label indigo</span>
+                <span class="badge badge-purple">Label purple</span>
+                <span class="badge badge-pink">Label pink</span>
+                <span class="badge badge-rose">Label rose</span>
+                <span class="badge badge-orange-dark">Label orange-dark</span>
+            </div>
+        </div>
+
+        <div>
+            <div class="space-x-2 space-y-2">
+                <div class="headline-2">Size lg</div>
+                <span class="badge badge-lg badge-gray">Label gray</span>
+                <span class="badge badge-lg badge-primary">Label primary</span>
+                <span class="badge badge-lg badge-error">Label error</span>
+                <span class="badge badge-lg badge-warning">Label warning</span>
+                <span class="badge badge-lg badge-success">Label success</span>
+                <span class="badge badge-lg badge-gray-blue">Label gray-blue</span>
+                <span class="badge badge-lg badge-blue-light">Label blue-light</span>
+                <span class="badge badge-lg badge-blue">Label blue</span>
+                <span class="badge badge-lg badge-indigo">Label indigo</span>
+                <span class="badge badge-lg badge-purple">Label purple</span>
+                <span class="badge badge-lg badge-pink">Label pink</span>
+                <span class="badge badge-lg badge-rose">Label rose</span>
+                <span class="badge badge-lg badge-orange-dark">Label orange-dark</span>
+            </div>
         </div>
     </div>
 </template>
--- example/pages/index.vue
+++ example/pages/index.vue
@@ -106,27 +106,5 @@
                 },
             ]"
         />
-
-        <div class="space-x-2">
-            <span class="badge badge-sm badge-gray">Label sm</span>
-            <span class="badge badge-gray">Label md</span>
-            <span class="badge badge-lg badge-gray">Label lg</span>
-        </div>
-
-        <div class="space-x-2 space-y-2">
-            <span class="badge badge-gray">Label gray</span>
-            <span class="badge badge-primary">Label primary</span>
-            <span class="badge badge-error">Label error</span>
-            <span class="badge badge-warning">Label warning</span>
-            <span class="badge badge-success">Label success</span>
-            <span class="badge badge-gray-blue">Label gray-blue</span>
-            <span class="badge badge-blue-light">Label blue-light</span>
-            <span class="badge badge-blue">Label blue</span>
-            <span class="badge badge-indigo">Label indigo</span>
-            <span class="badge badge-purple">Label purple</span>
-            <span class="badge badge-pink">Label pink</span>
-            <span class="badge badge-rose">Label rose</span>
-            <span class="badge badge-orange-dark">Label orange-dark</span>
-        </div>
     </div>
 </template>
