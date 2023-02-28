8dc8ed82725d435535b77359771be9d60c5c9fa8
Nhật Linh <linhln.flamedia@gmail.com>
2023-02-28 16:11:37 +0700

    push code

---
+++ example/pages/badge.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container py-10 space-y-10">
+        <div class="space-x-2 space-y-2">
+            <span class="badge badge-gray">Label gray</span>
+            <span class="badge badge-primary">Label primary</span>
+            <span class="badge badge-error">Label error</span>
+            <span class="badge badge-warning">Label warning</span>
+            <span class="badge badge-success">Label success</span>
+            <span class="badge badge-gray-blue">Label gray-blue</span>
+            <span class="badge badge-blue-light">Label blue-light</span>
+            <span class="badge badge-blue">Label blue</span>
+            <span class="badge badge-indigo">Label indigo</span>
+            <span class="badge badge-purple">Label purple</span>
+            <span class="badge badge-pink">Label pink</span>
+            <span class="badge badge-rose">Label rose</span>
+            <span class="badge badge-orange-dark">Label orange-dark</span>
+        </div>
+    </div>
+</template>
