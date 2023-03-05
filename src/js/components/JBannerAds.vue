0f8d235ee88be9a91635169b5c2845404262fab6
Nhật Linh <linhln.flamedia@gmail.com>
2023-03-05 09:54:58 +0700

    banner ads

---
+++ src/js/components/JBannerAds.vue
@@ -0,0 +1,21 @@
+<template>
+    <a v-if="item" :href="item.link" :target="item.target || '_blank'">
+        <JPicture
+            wrapperClass="picture-contain"
+            :src="item.image.url || item.image || '/placeholder.png'"
+            :mobileSrc="item.image_mobile.url || item.image_mobile || '/placeholder.png'"
+            :alt="item.image.alt || item.alt || item.title"
+        />
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            default: null,
+        },
+    },
+}
+</script>
