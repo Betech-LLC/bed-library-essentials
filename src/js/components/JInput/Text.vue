a8954b483c895456f3c681ff385fcb968a0c5d64
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-03-06 16:24:57 +0700

    push code

---
+++ src/js/components/JInput/Text.vue
@@ -0,0 +1,22 @@
+<template>
+    <input
+        class="input"
+        :value="modelValue"
+        @input="onInput"
+        :type="field.type"
+        :placeholder="field?.placeholder ?? field?.label"
+        v-bind="{ ...$attrs }"
+    />
+</template>
+<script>
+export default {
+    emits: ['update:modelValue'],
+    props: ['modelValue', 'field'],
+    methods: {
+        onInput(event) {
+            this.$emit('update:modelValue', event.target.value)
+        },
+    },
+}
+</script>
+<style lang=""></style>
