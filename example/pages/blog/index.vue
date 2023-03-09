c1d344dca1a304c72b7560006fb9ab8a1742a951
Nhật Linh <linhln.flamedia@gmail.com>
2023-03-09 08:25:15 +0700

    push code

--- example/pages/blog/index.vue
+++ example/pages/blog/index.vue
@@ -1,42 +1,369 @@
 <template>
-    <section class="container py-20 space-y-8">
-        <div class="headline-1">CARD BLOG COLUMN</div>
-        <div class="grid grid-cols-12 gap-4 xl:gap-8 md:gap-6">
-            <JCardBlog class="col-span-6 card-blog-xl" :item="post" />
-            <JCardBlog class="col-span-5 card-blog-lg" :item="post" />
-            <JCardBlog class="col-span-4" :item="post" />
-            <JCardBlog class="col-span-3 card-blog-sm" :item="post" />
-        </div>
-
-        <div class="headline-1">CARD BLOG ROW</div>
-        <div class="grid grid-cols-12 gap-4 xl:gap-8 md:gap-6">
-            <JCardBlog class="col-span-12 card-blog-row card-blog-xl" :item="post" />
-            <JCardBlog class="col-span-10 card-blog-row card-blog-lg" :item="post" />
-            <JCardBlog class="col-span-8 card-blog-row" :item="post" />
-            <JCardBlog class="col-span-6 card-blog-row card-blog-sm" :item="post" />
-            <JCardBlog class="col-span-4 card-blog-row card-blog-xs" :item="post" />
-        </div>
-    </section>
+    <JBlogCategory :bannerTop="bannerTop" :top_posts="top_posts" :posts_data="posts_data" :banner="banner" />
 </template>
 
 <script>
 export default {
     data() {
         return {
-            post: {
-                title: 'Những điều bạn cần biết về tối ưu nội dung SEO trong năm 2023',
-                description:
-                    'How do you create compelling presentations that wow your colleagues and impress your managers?',
-                published_at: '2023-02-02',
+            posts_data: [
+                {
+                    id: 85,
+                    title: 'Quae rerum non aut tenetur.',
+                    slug: 'quae-rerum-non-aut-tenetur',
+                    published_at: '2023-03-08',
+                    description:
+                        'A harum velit fugit culpa facilis repellat. Dolore ut quia placeat aspernatur sapiente in distinctio. Quia est voluptatem provident id.',
+                    category: {
+                        id: 24,
+                        title: 'Hic earum vel facilis.',
+                        slug: 'hic-earum-vel-facilis',
+                        image: { url: null, alt: 'Hic earum vel facilis.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                    },
+                    categories: [
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Quae rerum non aut tenetur.' },
+                    url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis/quae-rerum-non-aut-tenetur',
+                },
+                {
+                    id: 77,
+                    title: 'Nam laboriosam.',
+                    slug: 'nam-laboriosam',
+                    published_at: '2023-03-08',
+                    description:
+                        'Eveniet maxime velit modi rerum molestiae. Accusantium qui sequi sunt commodi doloribus cum.',
+                    category: {
+                        id: 3,
+                        title: 'Sit non.',
+                        slug: 'sit-non',
+                        image: { url: null, alt: 'Sit non.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/sit-non',
+                    },
+                    categories: [
+                        {
+                            id: 3,
+                            title: 'Sit non.',
+                            slug: 'sit-non',
+                            image: { url: null, alt: 'Sit non.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/sit-non',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Nam laboriosam.' },
+                    url: 'http://localhost:8000/tin-tuc/sit-non/nam-laboriosam',
+                },
+                {
+                    id: 44,
+                    title: 'Sit omnis sint vel.',
+                    slug: 'sit-omnis-sint-vel',
+                    published_at: '2023-03-08',
+                    description:
+                        'Ex quae explicabo et porro eveniet. Molestiae nulla quae accusantium necessitatibus sequi et beatae. Quas molestiae laboriosam in quis saepe dolores. Exercitationem tenetur totam voluptas similique.',
+                    category: {
+                        id: 16,
+                        title: 'Odit alias pariatur.',
+                        slug: 'odit-alias-pariatur',
+                        image: { url: null, alt: 'Odit alias pariatur.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/odit-alias-pariatur',
+                    },
+                    categories: [
+                        {
+                            id: 16,
+                            title: 'Odit alias pariatur.',
+                            slug: 'odit-alias-pariatur',
+                            image: { url: null, alt: 'Odit alias pariatur.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/odit-alias-pariatur',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Sit omnis sint vel.' },
+                    url: 'http://localhost:8000/tin-tuc/odit-alias-pariatur/sit-omnis-sint-vel',
+                },
+                {
+                    id: 85,
+                    title: 'Quae rerum non aut tenetur.',
+                    slug: 'quae-rerum-non-aut-tenetur',
+                    published_at: '2023-03-08',
+                    description:
+                        'A harum velit fugit culpa facilis repellat. Dolore ut quia placeat aspernatur sapiente in distinctio. Quia est voluptatem provident id.',
+                    category: {
+                        id: 24,
+                        title: 'Hic earum vel facilis.',
+                        slug: 'hic-earum-vel-facilis',
+                        image: { url: null, alt: 'Hic earum vel facilis.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                    },
+                    categories: [
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Quae rerum non aut tenetur.' },
+                    url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis/quae-rerum-non-aut-tenetur',
+                },
+                {
+                    id: 77,
+                    title: 'Nam laboriosam.',
+                    slug: 'nam-laboriosam',
+                    published_at: '2023-03-08',
+                    description:
+                        'Eveniet maxime velit modi rerum molestiae. Accusantium qui sequi sunt commodi doloribus cum.',
+                    category: {
+                        id: 3,
+                        title: 'Sit non.',
+                        slug: 'sit-non',
+                        image: { url: null, alt: 'Sit non.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/sit-non',
+                    },
+                    categories: [
+                        {
+                            id: 3,
+                            title: 'Sit non.',
+                            slug: 'sit-non',
+                            image: { url: null, alt: 'Sit non.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/sit-non',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Nam laboriosam.' },
+                    url: 'http://localhost:8000/tin-tuc/sit-non/nam-laboriosam',
+                },
+                {
+                    id: 44,
+                    title: 'Sit omnis sint vel.',
+                    slug: 'sit-omnis-sint-vel',
+                    published_at: '2023-03-08',
+                    description:
+                        'Ex quae explicabo et porro eveniet. Molestiae nulla quae accusantium necessitatibus sequi et beatae. Quas molestiae laboriosam in quis saepe dolores. Exercitationem tenetur totam voluptas similique.',
+                    category: {
+                        id: 16,
+                        title: 'Odit alias pariatur.',
+                        slug: 'odit-alias-pariatur',
+                        image: { url: null, alt: 'Odit alias pariatur.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/odit-alias-pariatur',
+                    },
+                    categories: [
+                        {
+                            id: 16,
+                            title: 'Odit alias pariatur.',
+                            slug: 'odit-alias-pariatur',
+                            image: { url: null, alt: 'Odit alias pariatur.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/odit-alias-pariatur',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Sit omnis sint vel.' },
+                    url: 'http://localhost:8000/tin-tuc/odit-alias-pariatur/sit-omnis-sint-vel',
+                },
+            ],
+
+            bannerTop: {
                 image: {
-                    url: 'https://via.placeholder.com/600x600',
-                    alt: 'Những điều bạn cần biết về tối ưu nội dung SEO trong năm 2023',
+                    url: 'https://via.placeholder.com/900x300',
+                    alt: 'banner blog category',
                 },
-                category: {
-                    title: 'SEO Strategy',
-                    url: this.$router.resolve({ name: 'blog-category', params: { category: 'seo-strategy' } }),
+            },
+
+            top_posts: [
+                {
+                    id: 191,
+                    title: 'Dolorem non.',
+                    slug: 'dolorem-non',
+                    published_at: '2023-03-08',
+                    description:
+                        'Explicabo reprehenderit temporibus nihil velit qui omnis amet. Vel id consequatur facilis sint unde. Aut officiis natus quia quia nisi similique. Voluptates est culpa dolore aut.',
+                    category: {
+                        id: 4,
+                        title: 'Dolorem cumque.',
+                        slug: 'dolorem-cumque',
+                        image: { url: null, alt: 'Dolorem cumque.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/dolorem-cumque',
+                    },
+                    categories: [
+                        {
+                            id: 4,
+                            title: 'Dolorem cumque.',
+                            slug: 'dolorem-cumque',
+                            image: { url: null, alt: 'Dolorem cumque.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/dolorem-cumque',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Dolorem non.' },
+                    url: 'http://localhost:8000/tin-tuc/dolorem-cumque/dolorem-non',
                 },
-                url: this.$router.resolve({ name: 'blog-slug', params: { slug: 'demo' } }),
+                {
+                    id: 162,
+                    title: 'Inventore id ad.',
+                    slug: 'inventore-id-ad',
+                    published_at: '2023-03-08',
+                    description:
+                        'Dolores in et aut aut magnam. Labore dolores velit quas ut magnam. Sed quaerat sunt quia quidem. Dolore porro quisquam quia cumque.',
+                    category: {
+                        id: 24,
+                        title: 'Hic earum vel facilis.',
+                        slug: 'hic-earum-vel-facilis',
+                        image: { url: null, alt: 'Hic earum vel facilis.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                    },
+                    categories: [
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Inventore id ad.' },
+                    url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis/inventore-id-ad',
+                },
+                {
+                    id: 117,
+                    title: 'Officiis.',
+                    slug: 'officiis',
+                    published_at: '2023-03-08',
+                    description:
+                        'Incidunt officiis omnis atque eius. Officia quia aut molestiae eius quisquam ut. Et molestias voluptatem iusto facere id qui.',
+                    category: {
+                        id: 2,
+                        title: 'Libero ducimus in.',
+                        slug: 'libero-ducimus-in',
+                        image: { url: null, alt: 'Libero ducimus in.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/libero-ducimus-in',
+                    },
+                    categories: [
+                        {
+                            id: 2,
+                            title: 'Libero ducimus in.',
+                            slug: 'libero-ducimus-in',
+                            image: { url: null, alt: 'Libero ducimus in.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/libero-ducimus-in',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Officiis.' },
+                    url: 'http://localhost:8000/tin-tuc/libero-ducimus-in/officiis',
+                },
+                {
+                    id: 102,
+                    title: 'Voluptas est maiores.',
+                    slug: 'voluptas-est-maiores',
+                    published_at: '2023-03-08',
+                    description:
+                        'Perspiciatis vel quaerat a in id voluptate. Voluptatem ad quia at esse blanditiis facere. Accusantium qui voluptas repudiandae et. Ad molestiae debitis ea.',
+                    category: {
+                        id: 8,
+                        title: 'Quidem sit.',
+                        slug: 'quidem-sit',
+                        image: { url: null, alt: 'Quidem sit.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/quidem-sit',
+                    },
+                    categories: [
+                        {
+                            id: 8,
+                            title: 'Quidem sit.',
+                            slug: 'quidem-sit',
+                            image: { url: null, alt: 'Quidem sit.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/quidem-sit',
+                        },
+                        {
+                            id: 24,
+                            title: 'Hic earum vel facilis.',
+                            slug: 'hic-earum-vel-facilis',
+                            image: { url: null, alt: 'Hic earum vel facilis.' },
+                            icon: null,
+                            url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                        },
+                    ],
+                    image: { url: null, alt: 'Voluptas est maiores.' },
+                    url: 'http://localhost:8000/tin-tuc/quidem-sit/voluptas-est-maiores',
+                },
+            ],
+
+            banner: {
+                id: 2,
+                title: '<p>Demo</p>',
+                description: null,
+                link: null,
+                target: '_self',
+                custom_fields: null,
+                image: { url: 'https://via.placeholder.com/300x400', alt: 'Demo' },
+                image_mobile: { url: null, alt: 'Demo' },
             },
         }
     },
---
+++ src/js/components/JBlogCategory.vue
@@ -0,0 +1,71 @@
+<template>
+    <main>
+        <JBanner class="banner-sm" v-if="bannerTop && bannerTop.image" :image="bannerTop.image">
+            <div class="container pt-8">
+                <div>Breadcrumb</div>
+                <h1 class="mt-6 text-white display-2">Tin tức</h1>
+            </div>
+        </JBanner>
+
+        <section class="py-16">
+            <div class="container">
+                <div
+                    v-if="top_posts && top_posts.length"
+                    class="grid grid-cols-2 pb-8 border-b border-gray-200 gap-x-8"
+                >
+                    <JCardBlog :item="top_posts[0]" />
+
+                    <div class="space-y-5">
+                        <JCardBlog
+                            v-for="(post, index) in top_posts.slice(1, 4)"
+                            :key="index"
+                            class="pb-5 border-b border-gray-200 card-blog-sm card-blog-row card-blog-no-description last:pb-0 last:border-none"
+                            :item="post"
+                        />
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-7 mt-8 gap-x-8">
+                    <div class="col-span-5 space-y-8">
+                        <JCardBlog
+                            v-for="(post, index) in posts_data"
+                            :key="index"
+                            class="card-blog-md card-blog-row"
+                            :item="post"
+                        />
+                    </div>
+                    <div class="col-span-2 xl:space-y-12 lg:space-y-8">
+                        <div class="space-y-2 xl:space-y-4 md:space-y-3">
+                            <h2 class="pb-1 text-gray-700 border-b border-gray-500 title-2">Lượt xem nhiều nhất</h2>
+                            <div
+                                class="max-lg:grid max-lg:grid-cols-2 max-md:grid-cols-1 max-lg:gap-[22px] max-md:gap-0 space-y-2 xl:space-y-4 md:space-y-3"
+                            >
+                                <JCardBlog
+                                    v-for="(item, index) in top_posts"
+                                    :key="index"
+                                    class="pb-4 border-b border-gray-200 card-blog-xs card-blog-row custom-image last:border-none last:pb-0 max-lg:border-none"
+                                    :item="item"
+                                />
+                            </div>
+                        </div>
+
+                        <div class="max-lg:hidden">
+                            <JBannerAds :item="banner" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import JBanner from '@core/components/JBanner.vue'
+import JCardBlog from '@core/components/JCardBlog.vue'
+import JBannerAds from '@core/components/JBannerAds.vue'
+
+export default {
+    components: { JBanner, JCardBlog, JBannerAds },
+    props: ['bannerTop', 'banner', 'top_posts', 'posts_data'],
+}
+</script>
