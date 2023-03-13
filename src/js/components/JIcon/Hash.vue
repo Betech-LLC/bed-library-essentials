235fd4db978ec636596e40d2c3552b1b471735c8
Nhật Linh <linhln.flamedia@gmail.com>
2023-03-13 11:56:21 +0700

    push code

---
+++ src/js/components/JIcon/Hash.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="40" height="40" rx="20" fill="#D1E9FF" />
+        <path
+            d="M17.9166 12.5L15.4166 27.5M24.5833 12.5L22.0833 27.5M27.0833 16.6667H12.9166M26.25 23.3333H12.0833"
+            stroke="#1570EF"
+            stroke-width="1.66667"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+</template>
---
+++ src/js/components/JPageTag.vue
@@ -0,0 +1,52 @@
+<template>
+    <main class="tag-page" v-if="tag">
+        <section class="tag-page-banner">
+            <div class="content">
+                <JBreadcrumb :items="breadcrumb">
+                    <template #icon>
+                        <JIconArrowRight />
+                    </template>
+                </JBreadcrumb>
+                <div class="head">
+                    <JIconHash />
+                    <h1 class="title">{{ tag.title }}</h1>
+                </div>
+            </div>
+        </section>
+
+        <section class="tag-page-body">
+            <div class="tag-page-title">Bài viết</div>
+            <div class="tag-page-wrap">
+                <div class="left">
+                    <div class="card-items">
+                        <JCardBlog
+                            v-for="(item, index) in posts_data"
+                            :key="index"
+                            class="card-blog-row card-item"
+                            :item="item"
+                        />
+                    </div>
+
+                    <div class="tag-page-button">
+                        <button class="btn-see-more">Xem thêm 16 bài viết</button>
+                    </div>
+                </div>
+
+                <div v-if="banner" class="right">
+                    <JBannerAds :item="banner" />
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import JCardBlog from '@core/components/JCardBlog.vue'
+import JBannerAds from '@core/components/JBannerAds.vue'
+import JIconHash from '@core/components/JIcon/Hash.vue'
+
+export default {
+    components: { JCardBlog, JBannerAds, JIconHash },
+    props: ['breadcrumb', 'posts_data', 'tag', 'banner'],
+}
+</script>
