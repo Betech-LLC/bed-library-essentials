12f17f51c36f605c8258733f0fa713afb4881571
Nhật Linh <linhln.flamedia@gmail.com>
2023-03-23 10:41:38 +0700

    push code

--- example/pages/project/[slug].vue
+++ example/pages/project/[slug].vue
@@ -1,3 +1,66 @@
 <template>
-    <section>Project detail</section>
+    <JPageProjectDetail :item="project" :breadcrumb="breadcrumb" />
 </template>
+<script>
+export default {
+    data() {
+        return {
+            project: {
+                id: 85,
+                title: 'Tòa nhà Landmark 81',
+                slug: 'toa-nha-landmark-81',
+                published_at: '2023-03-08',
+                description:
+                    'Landmark 81 - tòa nhà chọc trời cao nhất Việt Nam, đẳng cấp và hiện đại, trang bị nhiều tiện ích cao cấp và tọa lạc tại vị trí đắc địa... ',
+                location: 'TP.HCM',
+                progress: {
+                    key: 'success',
+                    value: 'Hoàn thành',
+                },
+                category: {
+                    id: 24,
+                    title: 'Hic earum vel facilis.',
+                    slug: 'hic-earum-vel-facilis',
+                    image: { url: null, alt: 'Hic earum vel facilis.' },
+                    icon: null,
+                    url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                },
+                categories: [
+                    {
+                        id: 24,
+                        title: 'Hic earum vel facilis.',
+                        slug: 'hic-earum-vel-facilis',
+                        image: { url: 'null', alt: 'Hic earum vel facilis.' },
+                        icon: null,
+                        url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis',
+                    },
+                ],
+                image: { url: 'https://via.placeholder.com/600x600', alt: 'Quae rerum non aut tenetur.' },
+                url: 'http://localhost:8000/tin-tuc/hic-earum-vel-facilis/quae-rerum-non-aut-tenetur',
+                content: `<p>T&ograve;a nh&agrave; Landmark 81 l&agrave; một t&ograve;a nh&agrave; chọc trời ở Th&agrave;nh phố Hồ Ch&iacute; Minh, Việt Nam. Với chiều cao 461,5 m&eacute;t (1,513 ft), đ&acirc;y l&agrave; t&ograve;a nh&agrave; cao nhất tại Việt Nam v&agrave; l&agrave; một trong những t&ograve;a nh&agrave; cao nhất ở ch&acirc;u &Aacute;. T&ograve;a nh&agrave; được ho&agrave;n th&agrave;nh v&agrave;o năm 2018 bởi C&ocirc;ng ty TNHH B&aacute;n lẻ Vincom.</p>
+                        <p>T&ograve;a nh&agrave; Landmark 81 c&oacute; tổng diện t&iacute;ch s&agrave;n l&ecirc;n đến 141,000 m&sup2; (1,520,000 sq ft) v&agrave; bao gồm 81 tầng v&agrave; 3 tầng hầm. T&ograve;a nh&agrave; được thiết kế theo phong c&aacute;ch hiện đại với những đường cong mềm mại v&agrave; cạnh sắc. Ngo&agrave;i ra, Landmark 81 cũng được trang bị hệ thống an ninh, điều h&ograve;a kh&ocirc;ng kh&iacute; v&agrave; &aacute;nh s&aacute;ng hiện đại.</p>
+                        <p><img src="https://via.placeholder.com/600x300"></p>
+                        <p>T&ograve;a nh&agrave; Landmark 81 cũng c&oacute; một số tiện &iacute;ch vượt trội như c&aacute;c căn hộ cao cấp với số lượng l&ecirc;n tới 1,000 căn hộ, khu mua sắm, nh&agrave; h&agrave;ng, khu vui chơi giải tr&iacute; v&agrave; hồ bơi. Tất cả c&aacute;c tiện &iacute;ch n&agrave;y được thiết kế độc đ&aacute;o v&agrave; sang trọng, tạo n&ecirc;n một kh&ocirc;ng gian sống đẳng cấp v&agrave; tiện nghi cho cư d&acirc;n v&agrave; du kh&aacute;ch.</p>
+                        <p>Vị tr&iacute; của t&ograve;a nh&agrave; Landmark 81 cũng l&agrave; một điểm nhấn quan trọng, nằm tại địa chỉ 208 Nguyễn Hữu Cảnh, phường 22, quận B&igrave;nh Thạnh. Vị tr&iacute; n&agrave;y gi&uacute;p t&ograve;a nh&agrave; c&oacute; được tầm nh&igrave;n tuyệt đẹp v&agrave; truy cập dễ d&agrave;ng đến c&aacute;c khu vực quan trọng kh&aacute;c trong th&agrave;nh phố.</p>
+                        <p><img src="https://via.placeholder.com/600x300"></p>
+                        <p>Với kiến tr&uacute;c đẳng cấp, tiện &iacute;ch đầy đủ v&agrave; vị tr&iacute; tuyệt vời, T&ograve;a nh&agrave; Landmark 81 đ&atilde; trở th&agrave;nh một biểu tượng đ&aacute;ng tự h&agrave;o của Th&agrave;nh phố Hồ Ch&iacute; Minh v&agrave; Việt Nam, thu h&uacute;t sự ch&uacute; &yacute; của rất nhiều người d&acirc;n v&agrave; du kh&aacute;ch đến tham quan v&agrave; trải nghiệm.</p>
+                        <p>T&ograve;a nh&agrave; Landmark 81 l&agrave; một biểu tượng đ&aacute;ng tự h&agrave;o của Th&agrave;nh phố Hồ Ch&iacute; Minh v&agrave; Việt Nam. Với chiều cao l&ecirc;n đến 461,5 m&eacute;t, đ&acirc;y l&agrave; một trong những t&ograve;a nh&agrave; cao nhất ở ch&acirc;u &Aacute;. T&ograve;a nh&agrave; được trang bị nhiều tiện &iacute;ch cao cấp như hồ bơi, khu mua sắm, nh&agrave; h&agrave;ng v&agrave; khu vui chơi giải tr&iacute;, tạo n&ecirc;n một kh&ocirc;ng gian sống đẳng cấp v&agrave; tiện nghi cho cư d&acirc;n v&agrave; du kh&aacute;ch. Vị tr&iacute; của t&ograve;a nh&agrave; cũng đắc địa, nằm tại trung t&acirc;m Th&agrave;nh phố Hồ Ch&iacute; Minh, gi&uacute;p cư d&acirc;n v&agrave; du kh&aacute;ch dễ d&agrave;ng truy cập đến c&aacute;c khu vực quan trọng kh&aacute;c trong th&agrave;nh phố. T&ograve;a nh&agrave; được thiết kế độc đ&aacute;o với những đường cong mềm mại v&agrave; cạnh sắc, tạo n&ecirc;n một kiến tr&uacute;c đẳng cấp v&agrave; hiện đại. Landmark 81 đ&atilde; trở th&agrave;nh một điểm đến hấp dẫn cho du kh&aacute;ch v&agrave; một biểu tượng của sự ph&aacute;t triển đ&ocirc; thị của Việt Nam.</p>`,
+            },
+
+            breadcrumb: [
+                {
+                    title: 'Trang chủ',
+                    link: '/',
+                },
+                {
+                    title: 'Dự án',
+                    link: '/project',
+                },
+                {
+                    title: 'Tòa nhà Landmark 81',
+                },
+            ],
+        }
+    },
+}
+</script>
---
+++ src/js/components/JPageProjectDetail.vue
@@ -0,0 +1,71 @@
+<template>
+    <main>
+        <section class="page-project-detail">
+            <JBreadcrumb :items="breadcrumb">
+                <template #icon>
+                    <JIconArrowRight />
+                </template>
+            </JBreadcrumb>
+
+            <div class="page-project-detail-meta-wrap">
+                <div class="page-project-detail-meta">
+                    <h1 class="page-project-detail-title">{{ item.title }}</h1>
+                    <div class="page-project-detail-information">
+                        <div class="page-project-detail-information-title">Thông tin dự án</div>
+                        <div class="page-project-detail-information-body">
+                            <div class="page-project-detail-information-items">
+                                <div class="item">
+                                    <div class="label">Tiến độ:</div>
+                                    <div class="title">Hoàn thành</div>
+                                </div>
+                                <div class="item">
+                                    <div class="label">Loại hình:</div>
+                                    <div class="title">Tòa nhà chọc trời (skyscraper)</div>
+                                </div>
+                            </div>
+                            <div class="page-project-detail-information-items">
+                                <div class="item">
+                                    <div class="label">Sản phẩm sử dụng:</div>
+                                    <div class="title">Apollo Silicone, Dowsil</div>
+                                </div>
+                                <div class="item">
+                                    <div class="label">Địa điểm:</div>
+                                    <div class="title">TP.HCM, Việt Nam</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page-project-detail-description">
+                Landmark 81 - tòa nhà chọc trời cao nhất Việt Nam, đẳng cấp và hiện đại, trang bị nhiều tiện ích cao cấp
+                và tọa lạc tại vị trí đắc địa. Đây là biểu tượng của sự phát triển đô thị Việt Nam và điểm đến hấp dẫn
+                cho du khách.
+            </div>
+
+            <div class="page-project-detail-image" :href="item.url">
+                <JPicture :src="item.image?.url" :alt="item.image?.alt || item.title"> </JPicture>
+            </div>
+
+            <div class="page-project-detail-body">
+                <div class="page-project-detail-share">
+                    <JSocialShare class="social-share-column" zaloOAId="1111640952861619960" />
+                </div>
+                <div class="page-project-detail-content" v-html="item.content"></div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import JCardProject from '@core/components/JCardProject.vue'
+import JBreadcrumb from '@core/components/JBreadcrumb.vue'
+import JSocialShare from '@core/components/JSocialShare.vue'
+
+export default {
+    components: { JCardProject, JBreadcrumb, JSocialShare },
+
+    props: ['item', 'breadcrumb'],
+}
+</script>
