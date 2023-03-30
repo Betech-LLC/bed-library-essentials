b8ec803441f75ac5ee062041c17a58e06c043523
Nhật Linh <linhln.flamedia@gmail.com>
2023-03-30 17:11:04 +0700

    push code

--- src/js/utils/check/index.js
+++ src/js/utils/check/index.js
@@ -1,13 +1,12 @@
 'use strict'
 
 function checkActiveUrl(url, currentPath) {
-    console.log(url)
-    console.log(currentPath)
     if (!currentPath || !url) return false
     const currentPathClear = currentPath.endsWith('/') ? currentPath.slice(0, -1) : currentPath
 
     try {
         new URL(url)
+        console.log(new URL(url))
         if (currentPathClear === new URL(url).pathname) {
             return true
         }
