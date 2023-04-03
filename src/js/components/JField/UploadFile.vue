0681a0369186efd2f9a15335df0bfb83d9ef79b4
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-04-03 13:34:29 +0700

    push code

--- src/js/components/JField/UploadFile.vue
+++ src/js/components/JField/UploadFile.vue
@@ -16,13 +16,13 @@ export default {
             const fileExtension = file.name.split('.').pop()
             const isTypeCorrect = ['doc', 'docx', 'pdf'].includes(fileExtension)
             let i = 0
-            let _size = file.size
+            let size = file.size
             let fSExt = new Array('Bytes', 'KB', 'MB', 'GB')
-            while (_size > 900) {
-                _size /= 1024
+            while (size > 900) {
+                size /= 1024
                 i++
             }
-            const sizeCalc = Math.round(_size * 100) / 100
+            const sizeCalc = Math.round(size * 100) / 100
             const exactSize = sizeCalc + ' ' + fSExt[i]
             const isSizeCorrect =
                 exactSize.includes('Bytes') || exactSize.includes('KB') || (exactSize.includes('MB') && sizeCalc <= 5)
