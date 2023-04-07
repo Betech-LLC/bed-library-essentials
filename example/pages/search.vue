06444a92bfef5190ae3c2c42755b0c24a3acfe1e
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-04-07 18:03:36 +0700

    push code

---
+++ example/pages/search.vue
@@ -0,0 +1,70 @@
+<template>
+    <JPageSearch :posts="posts" :products="posts" :keyword="keyword" :breadcrumb="breadcrumb">
+        <template #card-product="{ item }">
+            <JCardBlog class="card-item" :item="item" />
+        </template>
+        <template #card-post="{ item }">
+            <JCardBlog class="card-item" :item="item" />
+        </template>
+    </JPageSearch>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            post: {
+                id: 85,
+                title: 'Quae rerum non aut tenetur.',
+                slug: 'quae-rerum-non-aut-tenetur',
+                published_at: '2023-03-08',
+                description:
+                    'A harum velit fugit culpa facilis repellat. Dolore ut quia placeat aspernatur sapiente in distinctio. Quia est voluptatem provident id.',
+                category: {
+                    id: 24,
+                    title: 'Hic earum vel facilis.',
+                    slug: 'hic-earum-vel-facilis',
+                    image: { url: null, alt: 'Hic earum vel facilis.' },
+                    icon: null,
+                    url: '/blog/hic-earum-vel-facilis',
+                },
+                categories: [
+                    {
+                        id: 24,
+                        title: 'Hic earum vel facilis.',
+                        slug: 'hic-earum-vel-facilis',
+                        image: { url: null, alt: 'Hic earum vel facilis.' },
+                        icon: null,
+                        url: '/blog/hic-earum-vel-facilis',
+                    },
+                ],
+                image: { url: null, alt: 'Quae rerum non aut tenetur.' },
+                url: '/blog/hic-earum-vel-facilis/quae-rerum-non-aut-tenetur',
+            },
+        }
+    },
+
+    computed: {
+        posts() {
+            return this.renderMultipleItems(this.post)
+        },
+        keyword() {
+            return this.$route.query.keyword
+        },
+
+        breadcrumb() {
+            return [{ title: 'Trang chủ', link: '/' }, { title: `Kết quả tìm kiếm cho từ khóa ${this.keyword}` }]
+        },
+    },
+
+    methods: {
+        renderMultipleItems(item) {
+            let items = []
+            for (let index = 0; index < 50; index++) {
+                items = [...items, item]
+            }
+            return items
+        },
+    },
+}
+</script>
+<style lang=""></style>
---
+++ src/js/components/JPageSearch.vue
@@ -0,0 +1,56 @@
+<template>
+    <main>
+        <JBreadcrumb :items="breadcrumb" class="container mb-3 md:mb-4 xl:mb-6">
+            <template #icon>
+                <JIconArrowRight />
+            </template>
+        </JBreadcrumb>
+        <div class="pb-3 border-b border-b-gray-300">
+            <div class="container text-gray-900 label-1">
+                Tìm thấy
+                <a href="#search-product" class="text-primary hover:underline">{{ products.length }} sản phẩm</a>,
+                <a href="#search-post" class="text-primary hover:underline">{{ posts.length }} bài viết</a> phù hợp với
+                từ khóa “{{ keyword }}”
+            </div>
+        </div>
+        <div class="py-6 space-y-6 md:space-y-8 xl:space-y-12 md:py-8 xl:py-12">
+            <div id="search-product">
+                <JPagination title="Sản phẩm" :cols="5" :items="products">
+                    <template #card="{ item }">
+                        <slot name="card-product" :item="item" />
+                    </template>
+                </JPagination>
+            </div>
+            <div id="search-post">
+                <JPagination title="Bài viết" :cols="4" :items="posts">
+                    <template #card="{ item }">
+                        <slot name="card-post" :item="item" />
+                    </template>
+                </JPagination>
+            </div>
+        </div>
+    </main>
+</template>
+<script>
+import JBreadcrumb from '@core/components/JBreadcrumb.vue'
+import JPagination from '@core/components/JPagination.vue'
+import JIconArrowRight from '@core/components/JIcon/ArrowRight.vue'
+export default {
+    components: { JPagination, JBreadcrumb, JIconArrowRight },
+    props: {
+        keyword: { type: String },
+        breadcrumb: {
+            type: Array,
+        },
+        posts: {
+            type: Array,
+            default: [],
+        },
+        products: {
+            type: Array,
+            default: [],
+        },
+    },
+}
+</script>
+<style lang=""></style>
