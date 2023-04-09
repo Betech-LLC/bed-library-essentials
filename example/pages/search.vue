b5600511a649e8b4f000f4181cb06592298b07d9
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-04-09 15:58:33 +0700

    push code

--- example/pages/search.vue
+++ example/pages/search.vue
@@ -1,9 +1,9 @@
 <template>
-    <JPageSearch :keyword="keyword" :breadcrumb="breadcrumb" :options="items">
-        <template #[cardProduct]="{ item }">
+    <JPageSearch :keyword="keyword" :breadcrumb="breadcrumb" :options="[productOption, postOption]">
+        <template #[productOption.type]="{ item }">
             <JCardBlog class="card-item" :item="item" />
         </template>
-        <template #[cardPost]="{ item }">
+        <template #[postOption.type]="{ item }">
             <JCardBlog class="card-item" :item="item" />
         </template>
     </JPageSearch>
@@ -11,17 +11,23 @@
 <script>
 export default {
     data() {
-        return {
-            cardPost: 'card-post',
-            cardProduct: 'card-product',
-            post: {
-                id: 85,
-                title: 'Quae rerum non aut tenetur.',
-                slug: 'quae-rerum-non-aut-tenetur',
-                published_at: '2023-03-08',
-                description:
-                    'A harum velit fugit culpa facilis repellat. Dolore ut quia placeat aspernatur sapiente in distinctio. Quia est voluptatem provident id.',
-                category: {
+        let post = {
+            id: 85,
+            title: 'Quae rerum non aut tenetur.',
+            slug: 'quae-rerum-non-aut-tenetur',
+            published_at: '2023-03-08',
+            description:
+                'A harum velit fugit culpa facilis repellat. Dolore ut quia placeat aspernatur sapiente in distinctio. Quia est voluptatem provident id.',
+            category: {
+                id: 24,
+                title: 'Hic earum vel facilis.',
+                slug: 'hic-earum-vel-facilis',
+                image: { url: null, alt: 'Hic earum vel facilis.' },
+                icon: null,
+                url: '/blog/hic-earum-vel-facilis',
+            },
+            categories: [
+                {
                     id: 24,
                     title: 'Hic earum vel facilis.',
                     slug: 'hic-earum-vel-facilis',
@@ -29,44 +35,39 @@ export default {
                     icon: null,
                     url: '/blog/hic-earum-vel-facilis',
                 },
-                categories: [
-                    {
-                        id: 24,
-                        title: 'Hic earum vel facilis.',
-                        slug: 'hic-earum-vel-facilis',
-                        image: { url: null, alt: 'Hic earum vel facilis.' },
-                        icon: null,
-                        url: '/blog/hic-earum-vel-facilis',
-                    },
-                ],
-                image: { url: null, alt: 'Quae rerum non aut tenetur.' },
-                url: '/blog/hic-earum-vel-facilis/quae-rerum-non-aut-tenetur',
+            ],
+            image: { url: null, alt: 'Quae rerum non aut tenetur.' },
+            url: '/blog/hic-earum-vel-facilis/quae-rerum-non-aut-tenetur',
+        }
+        function renderMultipleItems(item) {
+            let items = []
+            for (let index = 0; index < 50; index++) {
+                items = [...items, item]
+            }
+            return items
+        }
+
+        let posts = renderMultipleItems(post)
+
+        return {
+            productOption: {
+                title: 'Sản phẩm',
+                id: 'search-product',
+                type: 'card-product',
+                items: posts,
+                cols: 5,
+            },
+            postOption: {
+                title: 'Bài viết',
+                id: 'search-post',
+                type: 'card-post',
+                items: posts,
+                cols: 4,
             },
         }
     },
 
     computed: {
-        items() {
-            return [
-                {
-                    title: 'Sản phẩm',
-                    id: 'search-product',
-                    type: this.cardProduct,
-                    items: this.posts,
-                    cols: 5,
-                },
-                {
-                    title: 'Bài viết',
-                    id: 'search-post',
-                    type: this.cardPost,
-                    items: this.posts,
-                    cols: 4,
-                },
-            ]
-        },
-        posts() {
-            return this.renderMultipleItems(this.post)
-        },
         keyword() {
             return this.$route.query.keyword
         },
