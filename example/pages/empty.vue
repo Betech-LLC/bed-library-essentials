e35addeaa9292d9ea3073729bc2b79be00c2a0a1
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-04-13 11:28:30 +0700

    push code

---
+++ example/pages/empty.vue
@@ -0,0 +1,8 @@
+<template>
+    <JEmpty type="cart" description="Chưa có câu hỏi thường gặp, quý khách vui lòng đặt câu hỏi hoặc quay lại sau.">
+        <button class="btn btn-primary">Mua sắm ngay</button>
+    </JEmpty>
+
+    <JEmpty type="vlog" description="Chúng tôi đang cập nhật video, quý khách vui lòng quay lại sau."> </JEmpty>
+    <JEmpty type="search" description="Chúng tôi đang cập nhật video, quý khách vui lòng quay lại sau."> </JEmpty>
+</template>
---
+++ src/js/components/JEmpty.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="py-6 space-y-4 md:space-y-6 xl:space-y-8 xl:py-12 md:py-8">
+        <div class="w-[260px] md:w-[300px] mx-auto">
+            <img :src="image || images[type]" />
+        </div>
+        <div class="max-w-[490px] w-full mx-auto text-center text-gray-700">
+            {{ description }}
+        </div>
+        <div class="flex justify-center">
+            <slot />
+        </div>
+    </div>
+</template>
+<script>
+import JPicture from '@core/components/JPicture.vue'
+export default {
+    components: {
+        JPicture,
+    },
+    props: {
+        type: { type: String, default: null }, // vlog | cart | product | faqs | project | job
+        image: { type: String, default: null },
+        description: {
+            type: String,
+            default: null,
+        },
+    },
+    data() {
+        return {
+            images: {
+                vlog: '../../images/empty-vlog.webp',
+                cart: '../../images/empty-cart.webp',
+                product: '../../images/empty-product.webp',
+                search: '../../images/empty-search.webp',
+                project: '../../images/empty-project.webp',
+                job: '../../images/empty-job.webp',
+            },
+        }
+    },
+}
+</script>
