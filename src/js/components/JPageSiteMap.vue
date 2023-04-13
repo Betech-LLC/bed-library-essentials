a0d5f981cc405b6397532e76a4bf95d4be9fe0b9
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-04-13 17:02:52 +0700

    push code

---
+++ src/js/components/JPageSiteMap.vue
@@ -0,0 +1,68 @@
+<template>
+    <main>
+        <section class="site-map">
+            <JBreadcrumb :items="breadcrumb">
+                <template #icon>
+                    <JIconArrowRight />
+                </template>
+            </JBreadcrumb>
+
+            <div class="site-map-wrapper">
+                <h1 class="site-map-title">SITE MAP</h1>
+                <!-- Group Config -->
+                <div class="site-map-group-config">
+                    <div v-for="(group, index) in groupConfig" :key="index">
+                        <h2 class="site-map-group-config-title">{{ group.title }}</h2>
+                        <ul>
+                            <li
+                                v-for="(node, indexNode) in group.nodes"
+                                :key="indexNode"
+                                class="site-map-group-config-node"
+                            >
+                                <JLink :href="node.link">
+                                    <h3>{{ node.title }}</h3></JLink
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Group Options -->
+                <div class="site-map-group-options">
+                    <div v-for="(group, index) in groupOptions" :key="index">
+                        <h2 class="site-map-group-options-title">{{ group.title }}</h2>
+                        <ul class="site-map-group-options-list">
+                            <li
+                                v-for="(node, indexNode) in group.nodes"
+                                :key="indexNode"
+                                class="site-map-group-options-node"
+                            >
+                                <JLink :href="node.link">
+                                    <h3>{{ node.title }}</h3>
+                                </JLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+<script>
+import JLink from '@core/components/JLink.vue'
+import JBreadcrumb from '@core/components/JBreadcrumb.vue'
+import JIconArrowRight from '@core/components/JIcon/ArrowRight.vue'
+export default {
+    components: { JLink, JBreadcrumb, JIconArrowRight },
+    props: {
+        breadcrumb: {
+            type: Array,
+        },
+        groupConfig: { type: Array, default: [] },
+        groupOptions: {
+            type: Array,
+            default: [],
+        },
+    },
+}
+</script>
