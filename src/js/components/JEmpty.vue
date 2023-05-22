7945ad24fc63456d6aecd476cf627a305a3f15d0
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-05-22 13:06:51 +0700

    push code

--- src/js/components/JEmpty.vue
+++ src/js/components/JEmpty.vue
@@ -2,10 +2,12 @@
     <div class="empty">
         <div class="empty-image">
             <slot name="image">
-                <template v-if="!isLoading">
-                    <img v-if="imageLinks[name]" :src="imageLinks[name]" />
-                    <img v-else src="../../images/empty-search.webp" />
-                </template>
+                <img v-if="name === 'empty-vlog'" src="../../images/empty-vlog.webp" />
+                <img v-else-if="name === 'empty-cart'" src="../../images/empty-cart.webp" />
+                <img v-else-if="name === 'empty-product'" src="../../images/empty-product.webp" />
+                <img v-else-if="name === 'empty-project'" src="../../images/empty-project.webp" />
+                <img v-else-if="name === 'empty-job'" src="../../images/empty-job.webp" />
+                <img v-else src="../../images/empty-search.webp" />
             </slot>
         </div>
         <div v-if="description" class="empty-description" v-html="description"></div>
@@ -18,40 +20,11 @@
 <script>
 export default {
     props: {
-        name: { type: String, default: null }, // empty-vlog | empty-cart | empty-product | empty-search | empty-project | empty-job
+        name: { type: String, default: null },
+        // empty-vlog | empty-cart | empty-product | empty-search | empty-project | empty-job
         description: {
             type: String,
             default: null,
-        },
-    },
-
-    data() {
-        return {
-            image: null,
-            isLoading: false,
-            namesDefault: ['empty-vlog', 'empty-cart', 'empty-product', 'empty-search', 'empty-project', 'empty-job'],
-            imageLinks: {
-                'empty-vlog': '../../images/empty-vlog.webp',
-                'empty-cart': '../../images/empty-cart.webp',
-                'empty-product': '../../images/empty-product.webp',
-                'empty-search': '../../images/empty-search.webp',
-                'empty-project': '../../images/empty-project.webp',
-                'empty-job': '../../images/empty-job.webp',
-            },
-        }
-    },
-
-    created() {
-        if (this.namesDefault.includes(this.name)) {
-            this.loadImage()
-        }
-    },
-    methods: {
-        async loadImage() {
-            this.isLoading = true
-            const response = await import(/* @vite-ignore */ `../../images/${this.name}.webp`)
-            this.image = response.default
-            this.isLoading = false
         },
     },
 }
