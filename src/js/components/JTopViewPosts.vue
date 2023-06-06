b7cfb75f62a79159103782ead433bc90f279a7f9
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-06-06 18:25:03 +0700

    push code

--- src/js/components/JTopViewPosts.vue
+++ src/js/components/JTopViewPosts.vue
@@ -3,7 +3,6 @@
         <div class="left">
             <JCardBlog class="card-item" :item="top_views[0]" />
         </div>
-
         <div class="right" v-if="top_views && top_views.slice(1, 6).length > 0">
             <div class="title">Bài viết xem nhiều</div>
             <div class="list">
