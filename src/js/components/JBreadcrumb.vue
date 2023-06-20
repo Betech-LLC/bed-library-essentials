b6e0670c3264b0a646325c4d57e1f826222d2289
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-06-20 16:15:08 +0700

    push code

--- src/js/components/JBreadcrumb.vue
+++ src/js/components/JBreadcrumb.vue
@@ -1,19 +1,30 @@
 <template>
     <nav v-if="items && items.length > 0" class="breadcrumb">
-        <template v-for="item in items">
+        <template v-for="(item, index) in items">
             <template v-if="item.link">
-                <JLink :href="item.link" class="breadcrumb-item" v-html="item.title"> </JLink>
-                <span v-if="$slots.icon" class="icon">
-                    <slot name="icon"></slot>
-                </span>
-                <span v-else class="icon">
+                <JLink :href="item.link" class="breadcrumb-item">
+                  <span v-html="item.title"></span>
+                </JLink>
+                <span class="icon">
+                  <slot name="icon">
                     <JIconSlash />
+                  </slot>
                 </span>
             </template>
             <template v-else>
+              <template  v-if="items.length === index + 1">
                 <h1 v-if="isH1" class="breadcrumb-item" v-html="item.title"></h1>
-                <span v-else class="breadcrumb-item" v-html="item.title"> </span
-            ></template>
+                <span v-else class="breadcrumb-item" v-html="item.title"> </span>
+              </template>
+              <template v-else>
+                <span class="breadcrumb-item" v-html="item.title"></span>
+                <span class="icon">
+                  <slot name="icon">
+                      <JIconSlash />
+                  </slot>
+                </span>
+              </template>
+           </template>
         </template>
     </nav>
 </template>
